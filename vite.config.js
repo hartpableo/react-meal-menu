054cfee08d87c59df674371aa89c05f598cf0b8f
Hart <hart.hpwebdev@gmail.com>
2024-02-04 21:29:08 +0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/react-meal-menu/"
+  base: "/react-meal-menu/",
+  plugins: [react()]
 })
